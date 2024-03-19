28cd051452313bbe61b4efbf74815e51b7cbb963
Karol <karolkaraszewski@gmail.com>
2024-03-19 19:14:37 +0100

    added Task 2.c

--- src/main/java/org/example/JSONPlaceholderFetcher.java
+++ src/main/java/org/example/JSONPlaceholderFetcher.java
@@ -16,7 +16,7 @@ public class JSONPlaceholderFetcher {
     public HttpResponse<String> getSinglePost(int id) {
 
         try {
-            HttpRequest request = HttpRequest.newBuilder(new URI(postURL + "/" + id)).GET().build();
+            HttpRequest request = HttpRequest.newBuilder(new URI(postURL + "/" + id)).GET().build(); // zapytac o skladnie!!!
             return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
@@ -26,14 +26,27 @@ public class JSONPlaceholderFetcher {
     public HttpResponse<String> getAllPosts() {
 
         try {
-            HttpRequest request = HttpRequest.newBuilder(new URI(postURL+"/")).GET().build();
+            HttpRequest request = HttpRequest.newBuilder(new URI(postURL + "/")).GET().build();
             return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public boolean addPost(String post) {
-        return false;
+    public HttpResponse<String> addPost(String post) {
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder(new URI(postURL + "/"))
+                    .POST(HttpRequest.BodyPublishers.ofString(post))
+                    .build();
+
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main {
@@ -11,7 +10,7 @@ public class Main {
         JSONPlaceholderFetcher postFetcher = new JSONPlaceholderFetcher();
 
         HttpResponse<String> response = postFetcher.getSinglePost(1);
-        System.out.println("TASK 1:");
+        System.out.println("TASK 2.a");
         System.out.println(response.statusCode());
         System.out.println(response.body());
         System.out.println();
@@ -19,12 +18,28 @@ public class Main {
         // Task 2.b
 
         HttpResponse<String> response1 = postFetcher.getAllPosts();
-        System.out.println("TASK 2");
+        System.out.println("TASK 2.b");
         System.out.println(response1.statusCode());
         System.out.println(response1.body());
         System.out.println();
 
+        // Task 2.c
+
+        System.out.println("TASK 2.c");
+        HttpResponse<String> response2 = postFetcher.addPost("{\n" +
+                "  \"userId\": 1,\n" +
+                "  \"id\": 1,\n" +
+                "  \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\n" +
+                "  \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\n" +
+                "}");
+        int answer = response2.statusCode();
+
+        if (answer == 200 || answer == 201) {
+            System.out.println("Operation has been successful!");
+        } else {
+            System.out.println("Operation has NOT been successful!");
+        }
+
+
     }
-
-
 }
